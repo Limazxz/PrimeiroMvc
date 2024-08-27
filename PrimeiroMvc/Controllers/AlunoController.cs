d6a6522c1473ea11f1c712687598b7e6fc17fd1e
Limazxz <goubulsegue@gmail.com>
2024-08-27 11:54:20 -0300

    iniciando projeto MVC com a listagem de alunos

---
+++ PrimeiroMvc/Controllers/AlunoController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PrimeiroMvc.Models;
+
+namespace PrimeiroMvc.Controllers
+{
+    public class AlunoController : Controller
+    {
+        public IActionResult Index()
+        {
+            Aluno a1 = new Aluno(1, "Pedro Lima", "silaelima557@gmail.com", 123456);
+            Aluno a2 = new Aluno(2, "Geromel", "gero.mel@gmail.com", 789102);
+            Aluno a3 = new Aluno(3, "Léo Pelé", "peo.lele@gmail.com", 341267);
+            Aluno a4 = new Aluno(4, "Alan Franco", "alan.franco23@gmail.com", 123456);
+            Aluno a5 = new Aluno(5, "Arboleda", "arboledazagtri@gmail.com", 298750);
+
+            List<Aluno> listaAlunos = new List<Aluno>();
+            listaAlunos.Add(a1);
+            listaAlunos.Add(a2);
+            listaAlunos.Add(a3);
+            listaAlunos.Add(a4);
+            listaAlunos.Add(a5);
+
+            return View(listaAlunos);
+        }
+        public IActionResult Cadastrar()
+        {
+            return View();
+        }
+    }
+}
---
+++ PrimeiroMvc/Models/Aluno.cs
@@ -0,0 +1,21 @@
+﻿namespace PrimeiroMvc.Models
+{
+    public class Aluno
+    {
+        public int AlunoId { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public int RM { get; set; }
+
+        //Modificar o metodo contrutor
+        //Inserir os dados que vieram por parametros no método contrutor bos atributos da Classe
+
+        public Aluno(int Id, string Nome, string Email, int RM)
+        {
+            this.AlunoId = Id;
+            this.Nome = Nome;
+            this.Email = Email;
+            this.RM = RM;
+        }
+    }
+}
